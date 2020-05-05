34e4a76a64b4a1673e8046445cdcfbf0d2a10f2f
Luke Shepherd <stnvlms@ucl.ac.uk>
2020-05-05 01:25:28 +0100

    pip files

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from setuptools import setup
 
 setup(
     name="vox",
-    version="0.1",
-    description="rotating and alineing voxel and stl objects",
+    version="0.1.0",
+    description="Package for rotating, aligning and plotting voxel and stl (bone) objects",
     url="http://github.com/lukemshepherd/vox",
     author="Luke M Shepherd",
     author_email="l.m.shepherd@outlook.com",
     license="MIT",
     packages=["vox"],
-    install_requires=["numpy", "math" "pandas", "scipy.io", "stl,", "quaternion"],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "numpy-stl",
+        "numpy-quaternion",
+        "scipy",
+        "mayavi",
+        "scikit-learn",
+    ],
     zip_safe=False,
 )
